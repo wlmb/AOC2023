5b26aba9f2aa9cfa6ec6ae236c160dc3fbf9dc47
Luis Mochan <mochan@fis.unam.mx>
2023-12-09 15:02:56 -0600

    Day 9

---
+++ 9b.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+use v5.36;
+use PDL;
+use PDL::NiceSlice;
+my $total=0;
+while(<>){
+    chomp;
+    $total += extrapolate(pdl($_));
+}
+say $total;
+
+sub extrapolate($v){
+    return 0 if ($v==0)->all;
+    return $v->at(0)-extrapolate($v(1:-1)-$v(0:-2));
+}
