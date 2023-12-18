f292afc477b0dfc5843f1e2b82eac7946cff9058
Luis Mochan <mochan@fis.unam.mx>
2023-12-18 09:55:28 -0600

    SOlve day 18

---
+++ 18b.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+use v5.36;
+use PDL;
+use PDL::NiceSlice;
+#my %dirs=(R=>pdl(1,0), U=>pdl(0,-1), L=>pdl(-1,0), D=>pdl(0,1));
+my @dirs=(pdl(1,0), pdl(0,1), pdl(-1,0), pdl(0,-1));
+my $first=my $current=pdl(0,0);
+my @border=($first);
+my @edges;
+while(<>){
+    chomp;
+    last if /^$/;
+    die "Wrong input" unless /([RULD])\s+(\d*)\s+\(#([0-9a-f]{5})([0-3])\)/;
+    my ($olddir, $olddist, $disthex, $dirnum)=($1, $2, $3,$4);
+    my $dist=hex $disthex;
+    $current= $current+(my $edge=$dist*$dirs[$dirnum]);
+    push @border, $current;
+    push @edges, $edge;
+}
+die "Not a cycle" unless ($current==$first)->all;
+my $border=pdl(@border);
+my $perimeter=pdl(@edges)->abs->sumover->sumover;
+my $area=cross($border(:,0:-2), $border(:,1:-1))->sumover->abs/2;
+my $result=$area+$perimeter/2+1;
+say $result;
+
+
+sub cross($p, $q){
+    return pdl($p((0))*$q((1))-$p((1))*$q((0)));
+}
