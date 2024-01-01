2c2bc5a175330c6a1eebf9d2a3ffcf212034436e
Luis Mochan <mochan@fis.unam.mx>
2024-01-01 09:38:35 -0600

    New solution to day 25 without human intervention

--- 25a.pl
+++ 25a.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/env perl
 use v5.36;
-my @deleted=qw(mhb zqg sjr jlt fjn mzb);
-#my @deleted=qw(hfx pzl bvb cmg nvd jqt);
-my %deleted=(@deleted, reverse @deleted);
+use List::Util qw(sum0);
+
+die "Usage. $0 N file to relax N times" unless @ARGV==2;
+my $Nrelax=shift;
 my %connections;
 while(<>){
     chomp;
@@ -10,27 +11,85 @@ while(<>){
     my @components=split /:?\s+/;
     my $first=shift @components;
     for(@components){
-	next if defined $deleted{$first} and $deleted{$first} eq $_ ;
 	push $connections{$first}->@*, $_;
 	push $connections{$_}->@*, $first;
     }
- }
-my $group1=$deleted[0];
-my $group2=$deleted[1];
-my @counts;
-for($group1, $group2){
-    my %visited;
+}
+my ($first, $last, $nodes)=search_diameter();
+my %voltage;
+my @nodes=@$nodes; # ordered from first to last
+$voltage{$_}=0 for @nodes;
+$voltage{$first}=1;
+$voltage{$last}=-1;
+relax() for (1..$Nrelax);
+my %edges;
+do {my $f=$_;do {my @s=sort($f, $_); $edges{"@s"}=abs($voltage{$f}-$voltage{$_})}
+		 for $connections{$f}->@*} for @nodes;
+my @edges=sort {$edges{$b} <=> $edges{$a}} keys %edges;
+# test triads of edges until graph disconnects
+my $Nedges=@edges;
+my ($i, $j, $k)=(0,1,2);
+while($k<$Nedges){
+    last if disconnect(map {$edges[$_]} ($i, $j, $k));
+    ++$i, next if $i+1<$j;
+    ++$j, $i=0, next if $j+1<$k;
+    ++$k, $i=0, $j=1;
+}
+
+sub relax(){
+    my $voltage1;
+    $voltage{$_}=sum0 (map {$voltage{$_}} $connections{$_}->@*)/$connections{$_}->@* for @nodes;
+    $voltage{$last}=-1;
+    $voltage{$_}=sum0(map {$voltage{$_}} $connections{$_}->@*)/$connections{$_}->@* for reverse @nodes;
+    $voltage{$first}=1;
+}
+
+sub search_diameter(){
+    my $start0=(keys %connections)[0]; #arbitrary starting site
+    my $start=antipode($start0);
+    my ($end, $nodes)=antipode($start);
+    return ($start, $end, $nodes);
+}
+sub antipode($start){
     my @pending;
-    push @pending, $_;
-    my $count=0;
-    while(@pending){
+    my %visited;
+    push @pending, $start;
+    my $last;
+    my @nodes;
+    while(@pending){ # search farthest from $start0 breath first search
 	my $current=shift @pending;
 	next if $visited{$current};
-	++$count;
 	$visited{$current}=1;
-	#$visited{$_} || push @pending, $_ for $connections{$current}->@*;
-	push @pending, $_ for $connections{$current}->@*;
+	push @nodes, $current;
+	$last=$current;
+	push @pending, $connections{$current}->@*;
     }
-    push @counts, $count;
+    return wantarray? ($last, [@nodes]):$last;
 }
-say $counts[0]*$counts[1];
+
+sub disconnect($p, $q, $r){
+    # try to disconnect graph by removing edges $p, $q and $r
+    my @deleted=map {split " "} ($p, $q, $r);
+    my %deleted=(@deleted, reverse @deleted);
+    my $group1=$deleted[0];
+    my $group2=$deleted[1];
+    my @counts;
+    for($group1, $group2){
+	my %visited;
+	my @pending;
+	push @pending, $_;
+	my $count=0;
+	while(@pending){
+	    my $current=shift @pending;
+	    next if $visited{$current};
+	    ++$count;
+	    $visited{$current}=1;
+	    #$visited{$_} || push @pending, $_ for $connections{$current}->@*;
+	    push @pending, $_
+		for grep {!($deleted{$current} && $deleted{$_})} $connections{$current}->@*;
+	}
+	return 0 if $count==@nodes; # didn't split
+	push @counts, $count;
+    }
+    say "$p $q $r ", $counts[0]*$counts[1];
+}
